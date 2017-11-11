8fd9f8e60c7168f4992ebf63d097341cbdebb403
Vaggelis Kapartzianis <vkapartzianis@gmail.com>
2017-11-11 15:46:24 +0200

    Re-authorize everything ssh during boot

--- zeppelin/entrypoint.sh
+++ zeppelin/entrypoint.sh
@@ -1,4 +1,9 @@
-service sshd start
+service ssh start
+ssh-keyscan -H -t rsa 127.0.0.1  >> ~/.ssh/known_hosts
+ssh-keyscan -H -t rsa localhost  >> ~/.ssh/known_hosts
+ssh-keyscan -H -t rsa $(hostname)  >> ~/.ssh/known_hosts
+yes|ssh-keygen -q -N "" -t rsa -f ~/.ssh/id_rsa
+cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
 
 export PATH=$PATH:/usr/local/hadoop/bin:/usr/local/hadoop/sbin:/usr/local/zeppelin/bin
 export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
