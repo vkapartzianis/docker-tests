5a1acbc7149f61caa654d4ba94bab782126ba0a9
Vaggelis Kapartzianis <vkapartzianis@gmail.com>
2017-12-21 20:44:10 +0200

    Use common zeppelin-site.xml if it exists

--- zeppelin/entrypoint.sh
+++ zeppelin/entrypoint.sh
@@ -26,5 +26,6 @@ hdfs dfs -mkdir -p /user/zeppelin
 [ -d /zeppelin ] || mkdir /zeppelin
 [ -f /etc/zeppelin/shiro.ini ] && ln -fs /etc/zeppelin/shiro.ini /usr/local/zeppelin/conf/shiro.ini
 [ -f /etc/zeppelin/notebook-authorization.json ] && ln -fs /etc/zeppelin/notebook-authorization.json /usr/local/zeppelin/conf/notebook-authorization.json
+[ -f /etc/zeppelin/zeppelin-site.xml ] && ln -fs /etc/zeppelin/zeppelin-site.xml /usr/local/zeppelin/conf/zeppelin-site.xml
 [ -f /usr/local/zeppelin/conf/shiro.ini ] && export ZEPPELIN_NOTEBOOK_PUBLIC="false"
 cd /zeppelin && zeppelin.sh
